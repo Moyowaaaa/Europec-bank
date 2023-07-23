6a1785f19c7a1879bd1685bd60858b7af026f04a
Moyowaaaa <moyowa7@icloud.com>
2023-07-23 01:34:14 +0100

    responsiveness

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+// import loconativeScroll from "loconative-scroll";
+
+
+
+
+
+
+
+// const scroll = new loconativeScroll({
+//     el: document.querySelector("[data-scroll-container]"),
+//     smooth: true,
+//     lerp: 0.07,
+//   });
+  
+//   setTimeout(() => {
+//     scroll.update();
+//   }, 1000);
+  
