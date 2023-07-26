617ee21fa1ef80e6c5d643b54b3ce14572f4d0bd
Moyowaaaa <moyowa7@icloud.com>
2023-07-26 02:21:02 +0100

    animations(M.V.P)

--- src/js/index.js
+++ src/js/index.js
@@ -26,111 +26,160 @@ requestAnimationFrame(scrollFn);
 var viewport = window.innerWidth;
 var mobile = 480;
 
+// ----top Section Animations --- //
 
-const animatedSvgs = document.querySelectorAll('.box')
-console.log(animatedSvgs)
+const navbar = document.querySelector(".navbar");
+const titleContainer = document.querySelector(
+  ".titleSection__page-container--container"
+);
+const titleTextContainer = document.querySelector(".title-cont");
+const partnersMarquee = document.querySelector(".partners");
+const titleTl = gsap.timeline();
 
-const svgsTl = gsap.timeline()
-animatedSvgs.forEach((anim) => {
-  intersectionObserver(anim, { threshold: 0.8 }).then(() => {
-      svgsTl.from(anim.children,{
-        opacity:0,
-        ease:"power3.inOut",
-        duration:1,
-        
-      })
-    })
-})
-const controlFeaturesContainer = document.querySelector('#controlFeatures')
-const controlFeaturesBullets = document.querySelectorAll('.bullet-point')
-
-console.log(controlFeaturesBullets)
-
-const controlFeaturesTl = gsap.timeline()
-
-
-
-controlFeaturesBullets.forEach((bullet) => {
-  intersectionObserver(bullet, { threshold: 0.8 }).then(() => {
-    controlFeaturesTl.from(bullet,{
-      opacity:0,
-      ease:"power3.inOut",
-      duration:0.4,
-      stagger:0.1
-    })
-  })
-})
-
-
-
-
-
-
-const titleHeader = document.querySelectorAll("#titleHead");
-console.log(titleHeader);
-
-titleHeader.forEach((t) => {
-  const line = Splitting({
-    target: t,
-    by: "lines",
-  });
-  line.forEach((splitResult) => {
-    const wrappedLines = splitResult.words
-      .map(
-        (wordsArr) => `
-               <span class="word_wrap">
-                     ${wordsArr.outerHTML}
-                </span>`
-      )
-      .join("");
-    splitResult.el.innerHTML = wrappedLines;
-  });
-
-  // gsap.set(t.querySelectorAll(".word"), {
-  //     yPercent: 15,
-  //     opacity: 0,
-  //     rotateX: 50,
-  //     transform: "translate(0,100%)"
-  //     // transformStyle: "preserve-3d",
-  // });
-  intersectionObserver(t, { threshold: 0.8 }).then(() => {
-    const elem = t.querySelectorAll(".word");
-    gsap.fromTo(
-      elem,
-      {
-        yPercent: 15,
-        opacity: 0,
-      
-        transform: "translate(0,100%)",
-      },
-      {
-        yPercent: 0,
-        opacity: 1,
-        // stagger: elem.length > 100 ? 0.02 : 0.03,
-        stagger:0.02,
-        // duration: elem.length > 100 ? 0.65 : 0.75,
-        duration:0.4,
-        ease: "easeOut",
-        transform: "translate(0,0)",
-      }
-    );
+gsap.from(navbar.children, {
+  yPercent: 200,
+  ease: "power3.inOut",
+  duration: 1.2,
+  opacity: 0,
+});
+intersectionObserver(titleTextContainer, { threshold: 0.8 }).then(() => {
+  gsap.from(titleTextContainer.children, {
+    yPercent: 200,
+    ease: "power3.inOut",
+    duration: 1.2,
+    opacity: 0,
   });
 });
 
+titleTl
+  .from(
+    titleContainer.children[1].children,
+    {
+      yPercent: 200,
+      ease: "power3.inOut",
+      duration: 1.2,
+      opacity: 0,
+    },
+    "<0.1"
+  )
+  .from(
+    titleContainer.children[2],
+    {
+      y: 200,
+      ease: "power3.inOut",
+      duration: 1.2,
+      opacity: 0,
+    },
+    "<0.1"
+  );
+
+gsap.from(partnersMarquee, {
+  xPercent: -50,
+  opacity: 0,
+  duration: 5,
+  ease: "power3.inOut",
+  overflowX: "hidden",
+  scrollTrigger: {
+    trigger: partnersMarquee,
+    scrub: 4,
+    start: "top bottom+=20%",
+    end: "center top+=20%",
+  },
+});
+
+const subContainer = document.querySelector(".subSection__section-container");
+const subTitleContainer = document.querySelector(
+  ".subSection__section-container--hero-text-container"
+);
+const subTl = gsap.timeline();
+
+console.log(subTitleContainer.children[1]);
+
+subTl.from(subTitleContainer.children[0].children, {
+  yPercent: 200,
+  ease: "power3.inOut",
+  duration: 1.8,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: subContainer,
+    scrub: 1,
+    start: "top bottom+=20%",
+    end: "center top+=50%",
+  },
+});
+
+// ---showcase --//
+
+const showcaseTitles = Array.from(document.querySelectorAll(".showcaseTitle"));
+const financeShowcaseContainer = document.querySelector(
+  ".showcaseSection__finance-showcase-container"
+);
+const animatedSvgs = document.querySelectorAll(".box");
+
+showcaseTitles.forEach((title) => {
+  gsap.from(title.children, {
+    yPercent: 200,
+    ease: "power3.inOut",
+    duration: 1.8,
+
+    opacity: 0,
+    scrollTrigger: {
+      trigger: title,
+      scrub: 2,
+      start: "top bottom+=20%",
+      end: "center top+=20%",
+    },
+  });
+});
+
+gsap.from(financeShowcaseContainer.children[0].children[1], {
+  x: 300,
+  opacity: 0,
+  duration: 1,
+  ease: "power3.inOut",
+  scrollTrigger: {
+    trigger: financeShowcaseContainer.children[0].children[1],
+  },
+});
+
+gsap.from([financeShowcaseContainer.children[1].children], {
+  y: 200,
+  ease: "power3.inOut",
+  duration: 1,
+  opacity: 0,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: financeShowcaseContainer.children[1].children[0],
+  },
+});
+
+const svgsTl = gsap.timeline();
+animatedSvgs.forEach((anim) => {
+  svgsTl.from(
+    anim.children,
+    {
+      opacity: 0,
+      ease: "power3.inOut",
+      duration: 1,
+      delay: 1.1,
+      scrollTrigger: {
+        trigger: anim,
+      },
+    },
+    "<0.1"
+  );
+});
+
+//  --showcase banner
+
 const jumbrtronPhone = document.querySelector(".showcaseJumbotron");
-
-console.log(jumbrtronPhone);
-
 const featuresShowcasePoints = Array.from(
   document.querySelectorAll(
     ".showcaseJumbotron__text-container--bullet-container__bullet"
   )
 );
 
-console.log(featuresShowcasePoints);
-
 const tl = gsap.timeline();
-
 intersectionObserver(jumbrtronPhone, { threshold: 0.1 }).then(() => {
   featuresShowcasePoints.forEach((showcase) =>
     tl.from(showcase, {
@@ -141,7 +190,6 @@ intersectionObserver(jumbrtronPhone, { threshold: 0.1 }).then(() => {
       y: 70,
     })
   );
-
   gsap.from(jumbrtronPhone.children[0], {
     y: 100,
     opacity: 0,
@@ -151,7 +199,6 @@ intersectionObserver(jumbrtronPhone, { threshold: 0.1 }).then(() => {
       trigger: jumbrtronPhone,
     },
   });
-
   gsap.from(jumbrtronPhone.children[1].children[0].children[0], {
     y: 100,
     opacity: 0,
@@ -164,11 +211,74 @@ intersectionObserver(jumbrtronPhone, { threshold: 0.1 }).then(() => {
   });
 });
 
+// ----
+
+//  ---Faq Section
+
+const faqSection = document.querySelector(".faqSection");
+
+gsap.from(faqSection.children, {
+  yPercent: 200,
+  opacity: 0,
+  ease: "power3.inOut",
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: faqSection.children,
+    scrub: 2,
+    // scrub:0.1,
+    // start: "top bottom+=20%",
+    // end: "center top+=20%",
+  },
+});
+// ---
+
+// ----quotes banner
+
+const quotesBanner = document.querySelector(".quoteJumbotron");
+
+gsap.from(quotesBanner.children[0].children, {
+  yPercent: 200,
+  opacity: 0,
+  ease: "power3.inOut",
+  duration: 1,
+  stagger: 0.1,
+});
+
+gsap.from(quotesBanner.children[1].children[0], {
+  width: "1rem",
+  duration: 2,
+  ease: "power3.inOut",
+  opacity: 0,
+});
+
+// ----
+
+// -----subscribe section
+
+const subscribeContainer = document.querySelector(
+  ".subscribeSection__container"
+);
+
+gsap.from(subscribeContainer.children[0].children, {
+  yPercent: 200,
+  ease: "power3.inOut",
+  duration: 1.8,
+
+  opacity: 0,
+  scrollTrigger: {
+    trigger: subscribeContainer.children[0].children,
+    scrub: 0.1,
+    start: "top bottom+=50%",
+    end: "center top+=20%",
+  },
+});
+
+
+///
 
 const year = document.querySelectorAll("#year");
 
 year.forEach((y) => {
   y.innerHTML = new Date().getFullYear();
 });
-
-
